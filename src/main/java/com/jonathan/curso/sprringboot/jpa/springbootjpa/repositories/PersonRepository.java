7f8435f9dde6cce73c85d50303d403aaba9dbdd0
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-04-25 16:15:57 -0600

    Metodos de consulta y consulta personalizada con @Query

--- src/main/java/com/jonathan/curso/sprringboot/jpa/springbootjpa/repositories/PersonRepository.java
+++ src/main/java/com/jonathan/curso/sprringboot/jpa/springbootjpa/repositories/PersonRepository.java
@@ -1,9 +1,29 @@
 package com.jonathan.curso.sprringboot.jpa.springbootjpa.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.jonathan.curso.sprringboot.jpa.springbootjpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+    
+    @Query("SELECT p FROM Person p WHERE p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
+    
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.name=?1")
+    List<Object[]> obtenerPersonData(String name);
+    
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.programmingLanguage=?1 and p.name=?2")
+    List<Object[]> obtenerPersonData(String programmingLanguage, String name);
+
+    
 }
